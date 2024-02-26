928485c1736197827e7bcbbbbce87ffb3db02b64
Glenn Guillard <glenn.guillard.pros@gmail.com>
2024-02-26 11:03:59 +0100

    Commencement du get API

---
+++ src/app/component/produit/produit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produit',
+  templateUrl: './produit.component.html',
+  styleUrls: ['./produit.component.css'],
+})
+export class ProduitComponent {}
