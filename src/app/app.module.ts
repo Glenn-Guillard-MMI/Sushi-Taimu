fca761a8d746aa1740b650b5f6d34fda57422b5b
Glenn Guillard <glenn.guillard.pros@gmail.com>
2024-02-26 12:27:06 +0100

    Modication de version suite à un problème

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ProduitComponent } from './component/produit/produit.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProduitComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
