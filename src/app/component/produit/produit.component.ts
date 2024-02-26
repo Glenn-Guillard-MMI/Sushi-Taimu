2cb1a326b1481f01474ab71386ebf6759c77a590
Glenn Guillard <glenn.guillard.pros@gmail.com>
2024-02-26 11:13:22 +0100

    Récupération API

--- src/app/component/produit/produit.component.ts
+++ src/app/component/produit/produit.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { GetAPIService } from 'src/app/service/get-api.service';
 
 @Component({
   selector: 'app-produit',
   templateUrl: './produit.component.html',
   styleUrls: ['./produit.component.css'],
 })
-export class ProduitComponent {}
+export class ProduitComponent implements OnInit {
+  constructor(private get: GetAPIService) {}
+  data = '';
+
+  ngOnInit(): void {
+    this.get.getAPIService().subscribe((contenue) => {
+      this.data = contenue;
+    });
+  }
+}
--- src/app/service/get-api.service.ts
+++ src/app/service/get-api.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GetAPIService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  public getAPIService(): Observable<any> {
+    return this.http.get(
+      'https://raw.githubusercontent.com/xtraide/testjson/main/test'
+    );
+  }
 }
