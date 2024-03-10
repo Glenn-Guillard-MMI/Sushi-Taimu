36bc182d883f319db22b8d8be12c4c9f2311be9a
Glenn Guillard <glenn.guillard.pros@gmail.com>
2024-03-10 16:07:30 +0100

    Modification du service panier (add) et ajout des test unitaire

--- src/app/service/panier.service.spec.ts
+++ src/app/service/panier.service.spec.ts
@@ -10,7 +10,19 @@ describe('PanierService', () => {
     service = TestBed.inject(PanierService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it("ADD: Ajout d'un élément ''", () => {
+    expect(service.add('')).toBeFalsy();
+  });
+
+  it("ADD: Ajout d'un élément undefined", () => {
+    expect(service.add(undefined)).toBeFalsy();
+  });
+
+  it("ADD: Ajout d'un élément réel sans quantité", () => {
+    expect(service.add('des pates')).toBeTruthy();
+  });
+
+  it("ADD: Ajout d'un élément réel avec quantité", () => {
+    expect(service.add('des pates', 520)).toBeTruthy();
   });
 });
